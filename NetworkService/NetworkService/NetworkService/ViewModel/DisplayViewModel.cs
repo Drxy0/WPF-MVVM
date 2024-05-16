665c7350db9bbbc4e8360d622ea19659c8fd8f7c
Bogdan <bogdan.drljevic123@gmail.com>
2024-05-16 12:51:16 +0200

    init

---
+++ NetworkService/NetworkService/NetworkService/ViewModel/DisplayViewModel.cs
@@ -0,0 +1,14 @@
+﻿using NetworkService.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkService.ViewModel
+{
+	public class DisplayViewModel : BindableBase
+	{
+
+	}
+}
---
+++ NetworkService/NetworkService/NetworkService/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,116 @@
+﻿using NetworkService.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace NetworkService.ViewModel
+{
+    public class MainWindowViewModel : BindableBase
+	{
+        private int count = 15; // Inicijalna vrednost broja objekata u sistemu
+                                // ######### ZAMENITI stvarnim brojem elemenata
+                                //           zavisno od broja entiteta u listi
+
+        public MyICommand<string> NavCommand { get; private set; }
+        public MyICommand<Window> CloseWindowCommand { get; private set; }
+        public EntitiesViewModel entitiesViewModel;
+        public DisplayViewModel displayViewModel;
+        public GraphViewModel graphViewModel;
+        
+        private BindableBase currentViewModel;
+
+
+        public MainWindowViewModel()
+        {
+            createListener(); //Povezivanje sa serverskom aplikacijom
+
+			NavCommand = new MyICommand<string>(OnNav);
+
+			entitiesViewModel = new EntitiesViewModel();
+            displayViewModel = new DisplayViewModel();
+            graphViewModel = new GraphViewModel();
+
+            CurrentViewModel = entitiesViewModel;
+		}
+
+        private void createListener()
+        {
+            var tcp = new TcpListener(IPAddress.Any, 25675);
+            tcp.Start();
+
+            var listeningThread = new Thread(() =>
+            {
+                while (true)
+                {
+                    var tcpClient = tcp.AcceptTcpClient();
+                    ThreadPool.QueueUserWorkItem(param =>
+                    {
+                        //Prijem poruke
+                        NetworkStream stream = tcpClient.GetStream();
+                        string incomming;
+                        byte[] bytes = new byte[1024];
+                        int i = stream.Read(bytes, 0, bytes.Length);
+                        //Primljena poruka je sacuvana u incomming stringu
+                        incomming = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+
+                        //Ukoliko je primljena poruka pitanje koliko objekata ima u sistemu -> odgovor
+                        if (incomming.Equals("Need object count"))
+                        {
+                            //Response
+                            /* Umesto sto se ovde salje count.ToString(), potrebno je poslati 
+                             * duzinu liste koja sadrzi sve objekte pod monitoringom, odnosno
+                             * njihov ukupan broj (NE BROJATI OD NULE, VEC POSLATI UKUPAN BROJ)
+                             * */
+                            Byte[] data = System.Text.Encoding.ASCII.GetBytes(count.ToString());
+                            stream.Write(data, 0, data.Length);
+                        }
+                        else
+                        {
+                            //U suprotnom, server je poslao promenu stanja nekog objekta u sistemu
+                            Console.WriteLine(incomming); //Na primer: "Entitet_1:272"
+
+                            //################ IMPLEMENTACIJA ####################
+                            // Obraditi poruku kako bi se dobile informacije o izmeni
+                            // Azuriranje potrebnih stvari u aplikaciji
+
+                        }
+                    }, null);
+                }
+            });
+
+            listeningThread.IsBackground = true;
+            listeningThread.Start();
+        }
+
+		public BindableBase CurrentViewModel
+		{
+			get { return currentViewModel; }
+			set
+			{
+				SetProperty(ref currentViewModel, value);
+			}
+		}
+		private void OnNav(string destination)
+		{
+			switch (destination)
+			{
+				case "entities":
+					CurrentViewModel = entitiesViewModel;
+					break;
+				case "display":
+					CurrentViewModel = displayViewModel;
+					break;
+				case "graph":
+					CurrentViewModel = graphViewModel;
+					break;
+			}
+		}
+	}
+}
